a79fee61fb8f8cd5ed93c371168b76eb2b18f335
Jack <jackaklassen.jak@gmail.com>
2023-05-22 14:59:41 -0700

    Added core file with plan for functions

---
+++ core/WebToEPub.py
@@ -0,0 +1,10 @@
+
+
+
+#BeautifulSoup grab Webpage
+
+#Check website against options (if alt-hist point grab webpage at all threadmarks etc)
+
+#make it apply formatting to grabed text
+
+#Turn to EPub and export.
